45b55c2f2c32bd5942409275d1edd26148a1f932
emperW <544094478@qq.com>
2018-11-12 15:22:01 +0800

    Base security operation

--- SafetyDemo/src/main/java/com/security/demo/Startermain.java
+++ SafetyDemo/src/main/java/com/security/demo/Startermain.java
@@ -1,4 +1,11 @@
 package com.security.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Startermain {
+    public static void main(String[] args) {
+        SpringApplication.run(Startermain.class,args);
+    }
 }
--- SafetyDemo/src/main/java/com/security/demo/controller/SecurityController.java
+++ SafetyDemo/src/main/java/com/security/demo/controller/SecurityController.java
@@ -1,4 +1,14 @@
 package com.security.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SecurityController {
+
+    @GetMapping("index.do")
+    public String index(){
+
+        return "index";
+    }
 }
