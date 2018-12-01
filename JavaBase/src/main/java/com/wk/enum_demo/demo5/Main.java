4336bdcabe1cb4ae60f9dfd368a5025dca2f2b82
emperwang <544094478@qq.com>
2018-12-01 10:57:47 +0800

    move file to javaparent

--- JavaBase/src/main/java/com/wk/enum_demo/demo5/Main.java
+++
@@ -1,9 +0,0 @@
-package com.wk.enum_demo.demo5;
-
-public class Main {
-    public static void main(String[] args) {
-        //直接调用复用方法
-        String s = ColorOverride.RED.toString();
-        System.out.println(s);
-    }
-}
--- JavaBase/src/main/java/com/wk/enum_demo/demo6/Main.java
+++
@@ -1,9 +0,0 @@
-package com.wk.enum_demo.demo6;
-
-public class Main {
-    public static void main(String[] args) {
-        String info = EnumInterface.GREEN.getInfo();
-        System.out.println(info);
-        System.out.println(EnumInterface.RED.getInfo());
-    }
-}
