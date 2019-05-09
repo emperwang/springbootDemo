3d09ba0d0c7bc643e606ad9815dd8d47dd7080a8
emperWang <544094478@qq.com>
2019-05-09 12:35:16 +0800

    remove java base project

--- JavaParent/CommonDemo/src/main/java/com/wk/data/DataWriter.java
+++
@@ -1,9 +0,0 @@
-package com.wk.data;
-
-import java.io.Closeable;
-
-public interface DataWriter extends Closeable{
-
-    void writeBytes(byte b[]);
-    void write(byte b[], int offet, int length);
-}
--- JavaParent/CommonModule/src/main/java/com/java/modules/sink/Sinker.java
+++
@@ -1,5 +0,0 @@
-package com.java.modules.sink;
-
-public interface Sinker<T> {
-    boolean drain(T t);
-}
