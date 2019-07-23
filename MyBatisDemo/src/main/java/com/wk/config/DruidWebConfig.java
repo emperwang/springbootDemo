86a5c7e371a96fae326dfb5f6c392822089c149d
emperwang <544094478@qq.com>
2019-07-23 11:12:04 +0800

    fix comment

--- MyBatisDemo/src/main/java/com/wk/config/DruidWebConfig.java
+++ MyBatisDemo/src/main/java/com/wk/config/DruidWebConfig.java
@@ -35,7 +35,7 @@ public class DruidWebConfig {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
         // 添加过滤规则
         filterRegistrationBean.addUrlPatterns("/*");
-        // 添加不需要忽略的格式信息
+        // 添加忽略的格式信息
         filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
         System.out.println("druid初始化成功");
         return filterRegistrationBean;
