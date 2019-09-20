066130080a8716465daa43577c4a99018428b03f
emperwang <544094478@qq.com>
2019-09-20 13:44:25 +0800

    generate primary key,and fix bean

--- MyBatisDemo/src/main/java/com/wk/Entity/UserBean.java
+++ MyBatisDemo/src/main/java/com/wk/Entity/UserBean.java
@@ -1,5 +1,12 @@
 package com.wk.Entity;
 
+import lombok.*;
+
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserBean {
     private Integer id;
 
@@ -8,36 +15,4 @@ public class UserBean {
     private Integer age;
 
     private String address;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address == null ? null : address.trim();
-    }
 }
