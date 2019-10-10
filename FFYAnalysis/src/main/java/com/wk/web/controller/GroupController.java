9b665cdae84aa5ceab0ad5fea90b78239952b93d
emperwang <544094478@qq.com>
2019-10-10 18:23:12 +0800

    toAdd and base groupAdd version

--- FFYAnalysis/src/main/java/com/wk/web/controller/GroupController.java
+++ FFYAnalysis/src/main/java/com/wk/web/controller/GroupController.java
@@ -1,9 +1,12 @@
 package com.wk.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+
+@Slf4j
 @Controller
 @RequestMapping("group")
 public class GroupController {
@@ -12,5 +15,30 @@ public class GroupController {
     public String groupIndex(){
 
         return "group/index";
+    }
+
+    @GetMapping("toAdd.do")
+    public String groupToAdd(){
+
+        return "group/add";
+    }
+    @PostMapping("groupAdd.do")
+    @ResponseBody
+    public String groupAddAction(String groupName,String month,String personCount){
+        log.info("receive param groupName="+groupName+", month="+month+", personCount="+personCount);
+        return "success";
+    }
+
+    @PostMapping(value = "groupDelete.do")
+    @ResponseBody
+    public String groupdelete(@RequestParam(required = true) Integer[] ids){
+        log.info("groupdelete param is:"+ Arrays.asList(ids));
+        return "success";
+    }
+
+    @GetMapping("update.do")
+    public String groupUpdate(){
+
+        return "group/add";
     }
 }
