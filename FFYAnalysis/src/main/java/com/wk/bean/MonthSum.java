15ac3141062cb15fbbc99f6aa4544a7aa6146f0a
emperwang <544094478@qq.com>
2019-10-10 18:28:44 +0800

    fix bean

--- FFYAnalysis/src/main/java/com/wk/bean/MonthSum.java
+++ FFYAnalysis/src/main/java/com/wk/bean/MonthSum.java
@@ -1,8 +1,12 @@
 package com.wk.bean;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
+@Getter
+@Setter
 @ToString
 public class MonthSum implements Serializable{
     private Integer id;
@@ -13,35 +17,11 @@ public class MonthSum implements Serializable{
 
     private Integer personCount;
 
-    public Integer getId() {
-        return id;
-    }
+    public MonthSum(){}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName == null ? null : groupName.trim();
-    }
-
-    public Integer getMonth() {
-        return month;
-    }
-
-    public void setMonth(Integer month) {
+    public MonthSum(String groupName,Integer month,Integer personCount){
+        this.groupName = groupName;
         this.month = month;
-    }
-
-    public Integer getPersonCount() {
-        return personCount;
-    }
-
-    public void setPersonCount(Integer personCount) {
         this.personCount = personCount;
     }
 }
