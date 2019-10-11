d31d0381b5ce966a20cd23f6448958401790e74e
emperWang <544094478@qq.com>
2019-10-11 22:02:24 +0800

    update sql

--- FFYAnalysis/src/main/resources/sql/create.sql
+++ FFYAnalysis/src/main/resources/sql/create.sql
@@ -5,3 +5,11 @@ CREATE TABLE `month_sum` (
   `person_count` int(11) default NULL COMMENT '月末人数',
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `lossrate` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL COMMENT '人名',
+  `startTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `endTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
