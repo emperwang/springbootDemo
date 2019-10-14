295f965d44b46f66d98730e2bb6e0058d2871f6a
emperwang <544094478@qq.com>
2019-10-14 12:01:12 +0800

    add comment

--- springbootLogback/src/main/java/com/wk/config/logBackConfig.java
+++ springbootLogback/src/main/java/com/wk/config/logBackConfig.java
@@ -6,6 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+/**
+ *  可以使用此方法在logback中设置想获取的属性
+ *  <define name="hostname" class="com.wk.config.logBackConfig" />
+ */
 @Slf4j
 public class logBackConfig extends PropertyDefinerBase {
     @Override
