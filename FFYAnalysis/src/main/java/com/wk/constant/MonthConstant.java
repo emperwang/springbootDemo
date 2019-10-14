476d103c1dc975a876d6a5d4873306389f6fb956
emperWang <544094478@qq.com>
2019-10-14 23:11:00 +0800

    Fix monthConstant

--- FFYAnalysis/src/main/java/com/wk/constant/MonthConstant.java
+++ FFYAnalysis/src/main/java/com/wk/constant/MonthConstant.java
@@ -2,30 +2,30 @@ package com.wk.constant;
 
 public class MonthConstant {
 
-    private String januaryStart = "1月期初";
-    private String februayStart = "2月期初";
-    private String marchStart = "3月期初";
-    private String aprilStart = "4月期初";
-    private String mayStart = "5月期初";
-    private String juneStart = "6月期初";
-    private String julyStart = "7月期初";
-    private String augustStart = "8月期初";
-    private String septemberStart = "9月期初";
-    private String octoberStart = "10月期初";
-    private String novemberStart = "11月期初";
-    private String decemberStart = "12月期初";
+    public static final String januaryStart = "1月期初";
+    public static final String februayStart = "2月期初";
+    public static final String marchStart = "3月期初";
+    public static final String aprilStart = "4月期初";
+    public static final String mayStart = "5月期初";
+    public static final String juneStart = "6月期初";
+    public static final String julyStart = "7月期初";
+    public static final String augustStart = "8月期初";
+    public static final String septemberStart = "9月期初";
+    public static final String octoberStart = "10月期初";
+    public static final String novemberStart = "11月期初";
+    public static final String decemberStart = "12月期初";
 
 
-    private String januaryEnd = "1月期末";
-    private String februayEnd = "2月期末";
-    private String marchEnd = "3月期末";
-    private String aprilEnd = "4月期末";
-    private String mayEnd = "5月期末";
-    private String juneEnd = "6月期末";
-    private String julyEnd = "7月期末";
-    private String augustEnd = "8月期末";
-    private String septemberEnd = "9月期末";
-    private String octoberEnd = "10月期末";
-    private String novemberEnd = "11月期末";
-    private String decemberEnd = "12月期末";
+    public static final String januaryEnd = "1月期末";
+    public static final String februayEnd = "2月期末";
+    public static final String marchEnd = "3月期末";
+    public static final String aprilEnd = "4月期末";
+    public static final String mayEnd = "5月期末";
+    public static final String juneEnd = "6月期末";
+    public static final String julyEnd = "7月期末";
+    public static final String augustEnd = "8月期末";
+    public static final String septemberEnd = "9月期末";
+    public static final String octoberEnd = "10月期末";
+    public static final String novemberEnd = "11月期末";
+    public static final String decemberEnd = "12月期末";
 }
