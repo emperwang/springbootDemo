d9215305a01034baae940c3c202e5988864405ef
emperwang <544094478@qq.com>
2019-10-15 17:50:07 +0800

    static resource delete depts and regions

--- FFYAnalysis/src/main/resources/static/function/depementFunc.js
+++ FFYAnalysis/src/main/resources/static/function/depementFunc.js
@@ -17,3 +17,43 @@ $(function () {
         url: "/deptdata/getDataGride.do"
     });
 })
+
+
+// 删除操作
+$('#depentment-delete-btn').click(function () {
+    var ids = $('#deptShowdata').datagrid('getSelections');
+    var idsArr = new Array();
+    if (ids == undefined || ids.length <= 0){
+        showMsg("提示","At least select one");
+        return;
+    }
+    for (var i=0; i<ids.length;i++) {
+        idsArr.push(ids[i].id);
+        console.log(idsArr);
+    }
+    batchDeleteGroup(idsArr);
+});
+// 批量删除操作
+function batchDeleteGroup(idsArr) {
+    $.ajax({
+        url: "/deptdata/deptDelete.do",
+        type: "POST",
+        data: {'ids':idsArr+''},
+        dataType: "text",
+        async: false,
+        success: function (date) {
+            console.log("delete msg :"+date);
+            showMsg("提示",date);
+            $('#groupdata').datagrid('reload');
+        },
+        error: function (date) {
+            alert("delete error");
+        }
+    });
+}
+
+
+// 重新加载
+$('#depentment-reload-btn').click(function () {
+    $('#deptShowdata').datagrid('reload');
+});
--- FFYAnalysis/src/main/resources/static/function/regionFunc.js
+++ FFYAnalysis/src/main/resources/static/function/regionFunc.js
@@ -15,3 +15,45 @@ $(function () {
         url: "/regiondata/getDataGride.do"
     });
 })
+
+
+
+// 删除操作
+$('#region-delete-btn').click(function () {
+    var ids = $('#regionShowdata').datagrid('getSelections');
+    var idsArr = new Array();
+    if (ids == undefined || ids.length <= 0){
+        showMsg("提示","At least select one");
+        return;
+    }
+    for (var i=0; i<ids.length;i++) {
+        idsArr.push(ids[i].id);
+    }
+    batchDeleteGroup(idsArr);
+});
+// 批量删除操作
+// data: ids: 7,8
+function batchDeleteGroup(idsArr) {
+    $.ajax({
+        url: "/regiondata/regionDelete.do",
+        type: "POST",
+        data: {'ids':idsArr+''},
+        dataType: "text",
+        async: false,
+        success: function (date) {
+            console.log("delete msg :"+date);
+            showMsg("提示",date);
+            $('#regionShowdata').datagrid('reload');
+        },
+        error: function (date) {
+            alert("delete error");
+        }
+    });
+}
+
+
+
+// 重新加载
+$('#region-reload-btn').click(function () {
+    $('#regionShowdata').datagrid('reload');
+});
