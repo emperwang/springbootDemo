5547042292e0cc5845e12cb3019a275801afa3f7
emperwang <544094478@qq.com>
2019-10-15 19:23:59 +0800

    toAdd and toUpdate

--- FFYAnalysis/src/main/java/com/wk/web/controller/DepentmentController.java
+++ FFYAnalysis/src/main/java/com/wk/web/controller/DepentmentController.java
@@ -1,6 +1,10 @@
 package com.wk.web.controller;
 
+import com.wk.bean.views.DepentmentVo;
+import com.wk.web.service.DepentmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,10 +12,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("dept")
 public class DepentmentController {
 
+    @Autowired
+    private DepentmentService depentmentService;
 
     @GetMapping(value = "index.do")
     public String toIndex(){
 
         return "depment/index";
+    }
+
+    @GetMapping(value = "toAdd.do")
+    public String toAdd(){
+
+        return "depment/add";
+    }
+
+
+    @GetMapping(value = "toUpdate.do")
+    public String toUpdate(Model model , Integer id){
+        DepentmentVo depVo = depentmentService.selectByid(id);
+        model.addAttribute("item",depVo);
+        return "depment/update";
     }
 }
