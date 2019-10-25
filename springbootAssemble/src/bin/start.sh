2b361f1eaa0c33bf1e127ad7ac8473523c12bb12
emperwang <544094478@qq.com>
2019-10-25 17:29:56 +0800

    fix start.sh

--- springbootAssemble/src/bin/start.sh
+++ springbootAssemble/src/bin/start.sh
@@ -1,17 +1,81 @@
 #!/bin/bash
-BASE=`pwd`
-JAR_NAME=${BASE}/../boot/fcaps-controller.jar
-MPATH=${BASE}/../config
+BASE=$(cd $(dirname $0);pwd)
+JAR_NAME=spring-boot-assembly.jar
+JAR_PATH=${BASE}/../boot/${JAR_NAME}
 LIB=${BASE}/../lib
-# 设置配置文件
-CONFIG_FILE=${BASE}/../config/CollectorResourceConfig.properties
-# application.yml  配置文件外置
-APP_FILE=${BASE}/../config/
-LOG=logs/runtime.log
-for i in `ls ${LIB}`
-do
-        MPATH=${MPATH}:$i
-done
-JAVA_ARG="-Xmx512m -Xms512m -server -Dconfig.path=${CONFIG_FILE} -cp "${MPATH}
+AGENT_JAR=${BASE}/../MyPerf4J-ASM-2.8.0.jar
+CONFIG_FILE=${BASE}/../MyPerf4J.properties
+CONFIG=${BASE}/../config/
+LOG=${BASE}/../logs/runtime.log
+JAVA_ARG="-Xmx512m -Xms512m -server -Dloader.path=.,${LIB},${CONFIG} -javaagent:${AGENT_JAR} -DMyPerf4JPropFile=${CONFIG_FILE}"
 
-nohup java ${JAVA_ARG} -jar ${JAR_NAME} -Dspring.profile.active=pro --spring.config.location=${APP_FILE} > ${LOG} 2>&1 &
+
+function usage(){
+        echo
+        echo "usage: $0 [start | stop | restart | status]"
+        echo
+}
+
+function start(){
+                echo
+        echo "starting app..."
+                echo
+        nohup java ${JAVA_ARG} -jar ${JAR_PATH} -Dspring.profile.active=pro > ${LOG} 2>&1&
+}
+
+
+function stop(){
+        PID=`ps -ef | grep -v grep | grep "$JAR_NAME"  | awk '{print $2}'`
+        if [ $PID -gt 0 ];then
+                echo
+                echo "stopping  app $JAR_NAME .."
+                echo
+                kill -9 $PID
+         else
+                echo
+                echo "$JAR_NAME already stopped ... "
+                echo
+         fi
+}
+function status(){
+         PID=`ps -ef | grep -v grep | grep "$JAR_NAME"  | awk '{print $2}'`
+         if [ $PID > 0 ];then
+                echo
+                echo "$JAR_NAME is running ..."
+                echo
+         else
+                echo
+                echo "$JAR_NAME stopped ... "
+                echo
+         fi
+}
+
+function restart(){
+        stop
+        sleep 1
+        start
+}
+function main(){
+        if [ $# != 1 ]; then
+                usage
+                exit 1
+        fi
+        case $1 in
+        'start')
+                start
+                ;;
+        'stop')
+                stop
+                ;;
+        'restart')
+                restart
+                ;;
+        'status')
+                status
+                ;;
+        *)
+                usage
+                ;;
+        esac
+}
+main $1
