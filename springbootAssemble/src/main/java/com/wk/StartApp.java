7d4df2e23e494922606b551d2dd5067fdb2e6da6
emperwang <544094478@qq.com>
2019-10-30 15:43:57 +0800

    Add ipv6

--- springbootAssemble/src/main/java/com/wk/StartApp.java
+++ springbootAssemble/src/main/java/com/wk/StartApp.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartApp {
     public static void main(String[] args) {
+        // 设置支持ipv6地址
+        System.setProperty("java.net.preferIPv6Addresses","true");
         SpringApplication.run(StartApp.class,args);
     }
 }
