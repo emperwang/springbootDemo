bbe2db4fa8e627f353c75a5a8d7552f8560d8906
emperWang <544094478@qq.com>
2020-02-18 22:20:33 +0800

    fix swagger

--- SwaggerDemo/src/main/java/com/swagger/demo/config/SwaggerConfig.java
+++ SwaggerDemo/src/main/java/com/swagger/demo/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.swagger.demo.config;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -20,7 +21,10 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo()) // api信息
                 .select()           //构建api选择器
-                .apis(RequestHandlerSelectors.basePackage("com.swagger.demo.controller"))             //api选择器选择api的包
+                //api选择器选择api的包
+                // .apis(RequestHandlerSelectors.basePackage("com.swagger.demo.controller"))
+                // 选择类上有 ApiOperation注解的方法
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())            //api选择器选择包路径下任务api显示在文档中
                 .build();  //创建文档
     }
@@ -30,10 +34,10 @@ public class SwaggerConfig {
                 .title("springboot 使用swagger2构建RESTFUL接口")
                 .description("接口描述")
                 .termsOfServiceUrl("termsOfServiceUrl")
-                .contact("new  contact")
                 .version("1.0")
-                .license("http://springfox.github.io/springfox/docs/current/")
-                .licenseUrl("http://springfox.github.io/springfox/docs/current/")
                 .build();
+                //.license("http://springfox.github.io/springfox/docs/current/")
+                //.licenseUrl("http://springfox.github.io/springfox/docs/current/")
+                //.build();
     }
 }
--- SwaggerDemo/src/main/java/com/swagger/demo/controller/SwaggerController.java
+++ SwaggerDemo/src/main/java/com/swagger/demo/controller/SwaggerController.java
@@ -1,9 +1,7 @@
 package com.swagger.demo.controller;
 
 import io.swagger.annotations.*;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Api(value = "用户接口",tags = {"dogapi"}) //接口简要标注,对中文支持不太好
@@ -20,18 +18,21 @@ public class SwaggerController {
      *  paramType 有多种,还有path  query body, form
      * @return
      */
-
-    @ApiImplicitParams({ //接口需要的参数描述
+    //接口需要的参数描述
+    @ApiImplicitParams({
             @ApiImplicitParam(paramType = "header",name = "Token",value = "token",
-                    dataType = "string",required = true,defaultValue = "123")
+                    dataType = "string",required = true,defaultValue = "123"),
+            @ApiImplicitParam(paramType = "body", name = "name",dataType = "string",
+            required = true,defaultValue = "zhangsan")
     })
-    //接口功能描述
-    @ApiOperation(value = "获取一直狗")
+    //接口功能描述 httpMethod  指定请求方式
+    @ApiOperation(value = "获取一直狗", httpMethod = "POST")
     //接口响应信息,这里定义了一个401,当出现401,接口返回的是自定的错误实例,当然可以定义多个
     @ApiResponses(value = {@ApiResponse(code = 401,message = "请求为通过")})
-    @RequestMapping("getdog.do")
-    public String getDog(String Token){
-        System.out.println(Token);
+    @PostMapping("getdog.do")
+    public String getDog(@RequestHeader("Token") String Token, @RequestBody String name){
+        System.out.println("token =" + Token);
+        System.out.println("name = "+name);
         return "dog";
     }
 
@@ -39,11 +40,11 @@ public class SwaggerController {
             @ApiImplicitParam(paramType = "header",name = "Token",value = "token",dataType = "String",
             required = true,defaultValue = "123")
     })
-    @ApiOperation(value = "创建一直狗")
+    @ApiOperation(value = "创建一直狗", httpMethod = "GET")
     @ApiResponses(value = {
             @ApiResponse(code = 401,message = "请求未通过认证")
     })
-    @RequestMapping(value = "/dog/{name}.do")
+    @GetMapping(value = "/dog/{name}.do")
     public String createGog(@ApiParam(defaultValue = "二哈") //ApiParam 和 RequestParam效果相同
                             @PathVariable("name") String name){
         System.out.println("create a dog named:"+name);
