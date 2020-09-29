39de929981978a27bcf2f2b25bf0816f9cb060b4
kai.wang <kai.l.wang@ericsson.com>
2020-09-29 10:19:38 +0800

    oauth2 with implicit mode

---
+++ springboot-spring-oauth2/Demo03-authorization-server-wjth-implicit/src/main/java/com/wk/AuthorizateImplicitStarter.java
@@ -0,0 +1,11 @@
+package com.wk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizateImplicitStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizateImplicitStarter.class,args);
+    }
+}
---
+++ springboot-spring-oauth2/Demo03-resource-server/src/main/java/com/wk/controller/CallbackController.java
@@ -0,0 +1,23 @@
+package com.wk.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.OAuth2ClientProperties;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class CallbackController {
+
+    @GetMapping("/callback")
+    public String login(@RequestParam("code") String code){
+        return "假装这里有一个页面";
+    }
+}
