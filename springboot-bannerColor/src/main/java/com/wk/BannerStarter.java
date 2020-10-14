5b69eb535e11bbb03344d511740d53efa05e69f5
kai.wang <kai.l.wang@ericsson.com>
2020-10-14 18:03:00 +0800

    banner color

---
+++ springboot-bannerColor/src/main/java/com/wk/BannerStarter.java
@@ -0,0 +1,16 @@
+package com.wk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: wk
+ * @Date: 2020/10/14 14:38
+ * @Description
+ */
+@SpringBootApplication
+public class BannerStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(BannerStarter.class, args);
+    }
+}
